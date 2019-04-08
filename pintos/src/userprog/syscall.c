f444f54eef25a6bc8c4f54c513d03396c7eda9e3
Arjun Gill <arjun.gill@berkeley.edu>
2019-04-08 18:01:26 +0000

    Added call to process_wait for SYS_WAIT in syscall handler and implemented sp validation.  Added filler cases for file I/O syscalls

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -12,8 +12,8 @@
 static void syscall_handler (struct intr_frame *);
 bool valid_string(char *str);
 bool valid_pointer(void *pointer, size_t len);
-void validate_pointer(void *pointer, size_t len);
-void validate_string(char *str);
+void validate_pointer(uint32_t *eax_reg, void *pointer, size_t len);
+void validate_string(uint32_t *eax_reg, char *str);
 
 
 void
@@ -27,21 +27,44 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   uint32_t* args = ((uint32_t*) f->esp);
   //printf("System call number: %d\n", args[0]);
-  if (args[0] == SYS_EXIT) {
-    f->eax = args[1];
-    printf("%s: exit(%d)\n", &thread_current ()->name, args[1]);
-    thread_exit();
-  } else if (args[0] == SYS_WRITE && args[1] == 1) {
-    /* Write syscall with fd set to 1, so write to stdout */
-    putbuf((void *) args[2], args[3]);
-    f->eax = args[3];
-  } else if (args[0] == SYS_PRACTICE) {
+
+  validate_pointer(&f->eax, args, sizeof(uint32_t));
+  
+  if (args[0] == SYS_PRACTICE) {
     f->eax = args[1] + 1;
   } else if (args[0] == SYS_HALT) {
     shutdown_power_off();
+  } else if (args[0] == SYS_EXIT) {
+    f->eax = args[1];
+    printf("%s: exit(%d)\n", &thread_current ()->name, args[1]);
+    thread_exit();
   } else if (args[0] == SYS_EXEC) {
     f->eax = process_execute((char *)args[1]);
-  }
+  } else if (args[0] == SYS_WAIT) {
+  	 f->eax = process_wait(args[1]);
+	} else if (args[0] == SYS_CREATE) {
+
+	} else if (args[0] == SYS_REMOVE) {
+
+	} else if (args[0] == SYS_OPEN) {
+		
+	} else if (args[0] == SYS_FILESIZE) {
+		
+	} else if (args[0] == SYS_READ) {
+		
+	} else if (args[0] == SYS_WRITE) {
+		if (args[1] == 1) {
+			/* Write syscall with fd set to 1, so write to stdout */
+	    putbuf((void *) args[2], args[3]);
+	    f->eax = args[3];
+		}
+	} else if (args[0] == SYS_SEEK) {
+		
+	} else if (args[0] == SYS_TELL) {
+		
+	} else if (args[0] == SYS_CLOSE) {
+		
+	}
 }
 
 bool valid_pointer(void *pointer, size_t len) {
@@ -64,14 +87,20 @@ bool valid_string(char *str) {
 	return true;
 }
 
-void validate_pointer(void *pointer, size_t len) {
+void validate_pointer(uint32_t *eax_reg, void *pointer, size_t len) {
 	if (!valid_pointer(pointer, len)) {
-		thread_exit();
+		*eax_reg = -1;
+		printf("%s: exit(%d)\n", &thread_current ()->name, -1);
+	  thread_exit ();
+	  NOT_REACHED ();
 	}
 }
 
-void validate_string(char *str) {
+void validate_string(uint32_t *eax_reg, char *str) {
 	if (!valid_string(str)) {
-		thread_exit();
+		*eax_reg = -1;
+		printf("%s: exit(%d)\n", &thread_current ()->name, -1);
+	  thread_exit ();
+	  NOT_REACHED ();
 	}
 }
