bd6e42bf6f9419ef004082682c4dd13f2ada627b
nicholasRuhman <nwruhman@berkeley.edu>
2019-05-07 21:28:23 +0000

    Working on checking multiply directories. Temporarily commented out try_get_dir on line 75 of filesys.c

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -6,6 +6,7 @@
 #include "filesys/free-map.h"
 #include "filesys/inode.h"
 #include "filesys/directory.h"
+#include "threads/thread.h"
 
 /* Partition that contains the file system. */
 struct block *fs_device;
@@ -28,7 +29,7 @@ filesys_init (bool format)
     do_format ();
 
   struct inode *root_node = inode_open(ROOT_DIR_SECTOR);
-  //thread_current()->
+  thread_current()->working_dir = root_node;
 
 
   free_map_open ();
@@ -70,11 +71,12 @@ filesys_create (const char *name, off_t initial_size, bool is_dir)
 struct file *
 filesys_open (const char *name)
 {
-  struct dir *dir = dir_open_root ();
+  char file_name[NAME_MAX + 1];
+  struct dir *dir = dir_open_root();//try_get_dir(name, file_name) == NULL?  
   struct inode *inode = NULL;
-
+  
   if (dir != NULL)
-    dir_lookup (dir, name, &inode);
+    dir_lookup(dir, file_name, &inode);
   dir_close (dir);
 
   return file_open (inode);
@@ -113,12 +115,40 @@ get_next_part(char part[NAME_MAX + 1], const char **srcp) {
 bool
 filesys_chdir(const char *syscall_arg)
 {
-  
+	return false;
 }
 
-void
-rel_to_abs(char *file) {
-	//TODO
+/* Attempt to find the directory where the file exists.
+   Return dir if it exists, else return NULL. */
+struct dir *
+try_get_dir(const char *file_path, char next_part[NAME_MAX + 1]) {
+	if (*file_path == '\0')
+		return NULL;
+	
+	//char next_part[NAME_MAX + 1];
+	struct inode *cur_inode = rel_to_abs(file_path);
+
+	while (get_next_part(next_part, &file_path) > 0 && cur_inode != NULL)
+	{
+		struct dir *cur_dir = dir_open(cur_inode);
+		if (dir_lookup(cur_dir, next_part, &cur_inode))
+			dir_close(cur_dir);
+		else
+			break;
+	}
+
+	if (inode_is_dir(cur_inode))
+		strlcpy(next_part, ".", sizeof(char) * 2);
+	return dir_open(cur_inode);
+}
+
+/* Get the correct directory inode according to the given file_path. */
+struct inode *
+rel_to_abs(const char *file_path)
+{
+	if (file_path[0] == '/')
+		return dir_get_inode(dir_open_root());
+	return dir_get_inode(thread_current()->working_dir);
 }
 
 
--- pintos/src/filesys/filesys.h
+++ pintos/src/filesys/filesys.h
@@ -17,5 +17,7 @@ bool filesys_create (const char *name, off_t initial_size, bool is_dir);
 struct file *filesys_open (const char *name);
 bool filesys_remove (const char *name);
 bool filesys_isdir (const char *name);
+struct dir *try_get_dir(const char *, char[]);
+struct inode *rel_to_abs(const char *);
 
 #endif /* filesys/filesys.h */
