bf51e576c597e4c0cf94d40928d5a5fd1c29a069
Emma Han <emma_han@berkeley.edu>
2019-05-10 07:16:40 +0000

    implemented clock algorithm in cache_read

--- pintos/src/filesys/buffer.c
+++ pintos/src/filesys/buffer.c
@@ -23,6 +23,8 @@ filesys_cache_init(void)
 {
   lock_init(&cache_lock);
   clock_index = 0;
+  cache_miss = 0;
+  cache_hit = 0;
 
   for (i=0 ;i < CACHE_BLOCKS; i++)
     lock_init(cache_blocks[i] -> &block_lock)
@@ -33,18 +35,41 @@ filesys_cache_init(void)
 void
 cache_read_at(block_sector_t sector, void void *buffer, off_t size, off_t block_ofs)
 {
-  cache_block *cache = NULL;
+  *void cached;
   for (i=0; i < CACHE_BLOCKS; i++)
+
     /* read data into buffer */
-    if (cache_blocks[i]->sector == sector)
+    if (cache_blocks[i]->valid && cache_blocks[i]->sector == sector)
+      cashed_blocks[i]-> lock_acquire(block_lock)
       memcpy (buffer, cache_blocks[i]->data, BLOCK_SECTOR_SIZE);
+      cached_blocks[i]->recently_used = true;
+      cashed_blocks[i]-> lock_release(block_lock);
+
+      cashed = true;
+      cache_hit ++;
       break;
 
-  // if (cache == NULL)
-  //
+  /* Run clock algorithm to find an entry */
+  // need to run the clock algorithm on cache_blocks until we find an entry to evict,
+  // then evict it and load the new entry into its place
+    while(!cashed) {
+      if (cache_blocks[clock_index]->recently_used) {
+          cache_blocks[clock_index]->recently_used = false;
+        } else {
+          cashed_blocks[clock_index]-> lock_acquire(block_lock)
+          memcpy (buffer, cache_blocks[i]->data, BLOCK_SECTOR_SIZE);
+          cached_blocks[clock_index]->recently_used = true;
+          cashed_blocks[clock_index]-> lock_release(block_lock);
+          cashed = true;
+        }
 
-
-
+      if (clock_index == CACHE_BLOCKS) {
+        i = 0
+      } else {
+        clock_index ++;
+      }
+      cache_miss ++;
+    }
 }
 
 void
