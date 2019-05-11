ddeeca61906e62fac2d9a692c37201ea938fc486
Emma Han <emma_han@berkeley.edu>
2019-05-11 03:56:51 +0000

    fixed errors and cache_flushed

--- pintos/src/filesys/buffer.c
+++ pintos/src/filesys/buffer.c
@@ -2,7 +2,7 @@
 #include "threads/synch.h"
 
 /* Buffer cache with a maximum capacity of 64 disk blocks */
-#define CACHE_BLOCKS 64;
+#define CACHE_BLOCKS 64
 
 struct cache_block *cache_blocks[CACHE_BLOCKS];   /* Array of cache_blocks */
 struct lock cache_lock;                           /* Lock for cache_block synchronization */
@@ -26,39 +26,42 @@ filesys_cache_init(void)
   cache_miss = 0;
   cache_hit = 0;
 
-  for (i=0 ;i < CACHE_BLOCKS; i++)
-    lock_init(cache_blocks[i] -> &block_lock)
+  int i;
+  for (i=0 ;i < CACHE_BLOCKS; i++) {
+    lock_init(&cache_blocks[i] -> block_lock);
     cache_blocks[i] -> valid = false;
-    cache_block[i] -> dirty = false;
+    cache_blocks[i] -> dirty = false;
+  }
 }
 
 void
-cache_read_at(block_sector_t sector, void void *buffer, off_t size, off_t block_ofs)
+cache_read_at(block_sector_t sector, void *buffer, off_t size, off_t block_ofs)
 {
-  *void cached;
+  bool cached;
+  int i;
   for (i=0; i < CACHE_BLOCKS; i++)
     /* read data into buffer */
     if (cache_blocks[i]->valid && cache_blocks[i]->sector == sector) {
-      cashed_blocks[i]-> lock_acquire(cashed_blocks[clock_index]->&block_lock)
+      lock_acquire(&cache_blocks[clock_index]->block_lock);
       memcpy (buffer, cache_blocks[i]->data, BLOCK_SECTOR_SIZE);
-      cached_blocks[i]->recently_used = true;
-      cashed_blocks[i]-> lock_release(cashed_blocks[clock_index]->&block_lock);
+      cache_blocks[i]->recently_used = true;
+      lock_release(&cache_blocks[clock_index]->block_lock);
 
-      cashed = true;
+      cached = true;
       cache_hit ++;
       break;
       }
 
   /* Run clock algorithm to find an entry */
-    while(!cashed) {
+    while(cached == false) {
       if (cache_blocks[clock_index]->recently_used) {
           cache_blocks[clock_index]->recently_used = false;
         } else {
-          cashed_blocks[clock_index]-> lock_acquire(cashed_blocks[clock_index]->&block_lock)
+          lock_acquire(&cache_blocks[clock_index]->block_lock);
           memcpy (buffer, cache_blocks[i]->data, BLOCK_SECTOR_SIZE);
-          cached_blocks[clock_index]->recently_used = true;
-          cashed_blocks[clock_index]-> lock_release(cashed_blocks[clock_index]->&block_lock);
-          cashed = true;
+          cache_blocks[clock_index]->recently_used = true;
+          lock_release(&cache_blocks[clock_index]->block_lock);
+          cached = true;
         }
 
       if (clock_index == CACHE_BLOCKS) {
@@ -125,7 +128,7 @@ cache_write_at(block_sector_t sector, const void *buffer, off_t size, off_t bloc
       cache_lock->lock_acquire(cached_blocks[clock_index]->&block_lock);
       //if evicted block is dirty, write it to disk
       if (cache_blocks[clock_index]->dirty == true) {
-        //write block to disk 
+        //write block to disk
       }
       //write changes to cache
       memcpy(cache_blocks[clock_index]->data, buffer, BLOCK_SECTOR_SIZE);
@@ -143,9 +146,13 @@ cache_write_at(block_sector_t sector, const void *buffer, off_t size, off_t bloc
 void
 cache_flush(void)
 {
-  // for (i = 0; i < CACHE_BLOCKS; i++) {
-  //   if (cache_blocks[i]->valid && cache_blocks[i]->dirty) {
-  //     block_write(fs_device, cache_blocks[i]->sector, )
-  //   }
-  // }
+  int i;
+  for (i = 0; i < CACHE_BLOCKS; i++) {
+    if (cache_blocks[i]->valid && cache_blocks[i]->dirty) {
+      lock_acquire(&cache_blocks[i]->block_lock);
+      block_write(fs_device, cache_blocks[i]->sector, cache_blocks[i]->data);
+      cache_blocks[i]->dirty = false;
+      lock_release(&cache_blocks[i]->block_lock);
+    }
+  }
 }
